bd1b619a260e871b96df603dbe4a62f976a963e6
EsedovCelal <esedov.celal@gmail.com>
2022-12-10 15:45:20 +0400

    bug fixed

--- javascript.js
+++ javascript.js
@@ -1,13 +1,13 @@
 const left_all = document.querySelectorAll(".valutes_left");
 const right_all = document.querySelectorAll(".valutes_right");
-const left_AZN = document.querySelector(".left_AZN");
-const left_USD = document.querySelector(".left_USD");
-const left_EUR = document.querySelector(".left_EUR");
-const left_GBP = document.querySelector(".left_GBP");
-const right_AZN = document.querySelector(".right_AZN");
-const right_USD = document.querySelector("right_USD");
-const right_EUR = document.querySelector(".right_EUR");
-const right_GBP = document.querySelector(".right_GBP");
+// const left_AZN = document.querySelector(".left_AZN");
+// const left_USD = document.querySelector(".left_USD");
+// const left_EUR = document.querySelector(".left_EUR");
+// const left_GBP = document.querySelector(".left_GBP");
+// const right_AZN = document.querySelector(".right_AZN");
+// const right_USD = document.querySelector("right_USD");
+// const right_EUR = document.querySelector(".right_EUR");
+// const right_GBP = document.querySelector(".right_GBP");
 const left_input = document.querySelector(".left_input");
 const right_input = document.querySelector(".right_input");
 
