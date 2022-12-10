614d967425b671c80f90792f75b45e161dfa0312
EsedovCelal <esedov.celal@gmail.com>
2022-12-10 11:11:08 +0400

    build js

---
+++ javascript.js
@@ -0,0 +1,13 @@
+const left_AZN = document.querySelector(".left_AZN");
+const left_USD = document.querySelector(".left_USD");
+const left_EUR = document.querySelector(".left_EUR");
+const left_GBP = document.querySelector(".left_GBP");
+const right_AZN = document.querySelector(".right_AZN");
+const right_USD = document.querySelector("right_USD");
+const right_EUR = document.querySelector(".right_EUR");
+const right_GBP = document.querySelector(".right_GBP");
+console.log(left_AZN.innerText);
+
+left_AZN.addEventListener("click", () => {
+  console.log("salam");
+});
