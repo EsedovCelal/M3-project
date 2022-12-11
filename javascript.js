0b515f2e0c9c08b8693fe2b69b33086992558e45
EsedovCelal <107685956+EsedovCelal@users.noreply.github.com>
2022-12-11 20:55:49 +0400

    build js

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 const left_all = document.querySelectorAll(".valutes_left");
 const right_all = document.querySelectorAll(".valutes_right");
-const left_input = document.querySelector(".left_input");
-const right_input = document.querySelector(".right_input");
+var selected_left = "AZN";
+var selected_right = "USD";
 function left(selected) {
   const right_innerText = document.querySelector(
     ".right .valutes_right .active"
@@ -11,6 +11,7 @@ function left(selected) {
   )
     .then((link) => link.json())
     .then((link) => {
+      let left_Input_p = document.querySelector(".left .left_input_div p");
       const left_Input_Value = document.querySelector(
         ".left .left_input_div .left_input"
       ).value;
@@ -20,6 +21,7 @@ function left(selected) {
       right_Input_Value.value = (
         +left_Input_Value * link.rates[right_innerText]
       ).toFixed(2);
+      left_Input_p.innerText = `1 ${selected} = ${link.rates[right_innerText]} ${right_innerText}`;
     });
 }
 function right(selected) {
@@ -31,15 +33,16 @@ function right(selected) {
   )
     .then((link) => link.json())
     .then((link) => {
-      const right_Input_Value = document.querySelector(
+      const rate = link.rates[left_innerText];
+      let right_Input_p = document.querySelector(".right .right_input_div p");
+      let right_Input_Value = document.querySelector(
         ".right .right_input_div .right_input"
-      ).value;
-      const left_Input_Value = document.querySelector(
+      );
+      let left_Input_Value = document.querySelector(
         ".left .left_input_div .left_input"
       );
-      right_Input_Value = (
-        +left_Input_Value / link.rates[left_innerText]
-      ).toFixed(2);
+      left_Input_Value.value = (+right_Input_Value.value * rate).toFixed(2);
+      right_Input_p.innerText = `1 ${selected} = ${link.rates[left_innerText]} ${left_innerText}`;
     });
 }
 left_all.forEach((item) => {
@@ -48,8 +51,8 @@ left_all.forEach((item) => {
       .querySelector(".show_valuta_place .left .active")
       .classList.remove("active");
     e.target.classList.add("active");
-    const selected = e.target.innerText;
-    left(selected);
+    selected_left = e.target.innerText;
+    left(selected_left);
   });
 });
 right_all.forEach((item) => {
@@ -58,7 +61,22 @@ right_all.forEach((item) => {
       .querySelector(".show_valuta_place .right .active")
       .classList.remove("active");
     e.target.classList.add("active");
-    const selected = e.target.innerText;
-    right(selected);
+    selected_right = e.target.innerText;
+    right(selected_right);
   });
+});
+left(selected_left);
+right(selected_right);
+const left_input = document.querySelector(".left .left_input_div .left_input");
+left_input.addEventListener("keyup", (event) => {
+  left(selected_left);
+  console.log(selected_left);
+});
+
+const right_input = document.querySelector(
+  ".right .right_input_div .right_input"
+);
+right_input.addEventListener("keyup", (event) => {
+  right(selected_right);
+  console.log(selected_right);
 });
